7acc53eff141454b4dfc975a9cf836c2f98b7db7
uttamsdev <uttamsdev@gmail.com>
2024-07-17 12:59:13 +0600

    vite project setup and shadcn installation

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+    <Button>Testing</Button>
+    </>
+  )
+}
+
+export default App
